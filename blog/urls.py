e743efd2ee21f4bfae64e3eee112a99aeecb0c8d
benjamin.kary@gmail.com <benjamin.kary@gmail.com>
2017-07-28 06:41:31 -0700

    basic blogging capability complete

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.landing_page, name='landing_page'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+]
